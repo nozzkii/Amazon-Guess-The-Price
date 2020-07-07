71a2d99191b757440a07d00c88058798a88e049d
Anhtai <at@studioneun.de>
2020-07-07 22:43:06 +0200

    First commit

---
+++ app/api.py
@@ -0,0 +1,22 @@
+# Product Service
+
+# Import framework
+from flask import Flask, render_template, redirect, url_for
+
+# Instantiate the app
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/lobby")
+def lobby():
+    return render_template("lobby.html")
+
+@app.route("/letsplay")
+def letsplay():
+    return render_template("letsplay.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
