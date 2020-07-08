e392c25f696c900617a50170a259ca69e1f380c0
Anhtai <at@studioneun.de>
2020-07-08 21:23:07 +0200

    changed frontend and session query

--- app/api.py
+++ app/api.py
@@ -15,18 +15,26 @@ group = [
 ]
 
 @app.route("/")
-@app.route("/home")
+def redirecthome():
+    return redirect(url_for("home"))
+@app.route("/home", methods=["POST", "GET"])
 def home():
-    return render_template("index.php", group=group)
-
-@app.route("/lobby", methods=["POST", "GET"])
-def lobby():
     if request.method == "POST":
         user = request.form["nm"]
         session["user"] = user
         return redirect(url_for("user"))
     else:
-        return render_template("lobby.php")
+        return render_template("index.php", group=group)
+
+@app.route("/lobby")
+def lobby():
+    if "user" in session:
+        user = session["user"]
+        message = f"<p>You are logged in as {user}</p>"
+        return render_template("lobby.php", message=message)
+    else:
+        message = f"<p>You are not logged in</p>"
+        return render_template("lobby.php", message=message)
 
 @app.route("/letsplay")
 def letsplay():
@@ -36,14 +44,14 @@ def letsplay():
 def user():
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        return f"<h1>You are logged in as {user}</h1>"
     else:
         return redirect(url_for("lobby"))
 
-@app.route("/logout")
+@app.route("/logout", methods = ['POST', 'GET'])
 def logout():
     session.pop("user", None)
-    return redirect(url_for("lobby"))
+    return redirect(url_for("home"))
 
 @app.route('/setcookie', methods = ['POST', 'GET'])
 def setcookie():
@@ -52,11 +60,15 @@ def setcookie():
     resp = make_response(render_template('readcookie.html'))
     resp.set_cookie('userID', user)
     return resp
+    redirect(url_for("home"))
 
-@app.route('/getcookie')
+@app.route('/getcookie', methods = ['POST', 'GET'])
 def getcookie():
-    name = request.cookies.get('userID')
-    return '<h1>welcome ' + name + '</h1>'
+    if request.cookies.get('userID') != "":
+        name = request.cookies.get('userID')
+        return f'<h1>You are logged in as {name}</h1>'
+    else:
+        return redirect(url_for("home"))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- app/templates/index.php
+++ app/templates/index.php
@@ -8,33 +8,48 @@ $error='';
         <link rel="stylesheet" href="{{ url_for('static', filename='css/main.css') }}">
     </head>
     <body>
+      <div class="site-name">
       <h1>Guess The Price</h1>
-      {% for post in posts %}
-      <h1>hi</h1>
-      <p>By {{post.author}}</p>
-      {% endfor %}
-      <div id="wrap">
-<div id="regbar">
-  <div id="navthing">
-    <h2><a href="#" id="loginform">Login</a> | <a href="#">Register</a></h2>
-  <div class="login">
-    <div class="arrow-up"></div>
-    <div class="formholder">
-      <div class="randompad">
-         <fieldset>
-           <label name="email">Email</label>
-           <input type="email" value="example@example.com" />
-           <label name="password">Password</label>
-           <input type="password" />
-           <input type="submit" value="Login" />
-
-         </fieldset>
       </div>
+      <div class="section">
+      <div class="row, regbar">
+      <div class="column">
+      <h2>Session Creator</h2>
+      <form action="#" method="POST" class=formholder"">
+      <p>Name:</p>
+      <input type=text name="nm"/>
+      <input type=submit value="Login"/>
+      </form>
+      </div>
+      <div class="column">
+      <h2>Cookie Creator</h2>
+      <form action = "/setcookie" method = "POST">
+      <p>Enter User-ID</p>
+      <input type = 'text' name = 'ck'/>
+      <input type = 'submit' value = 'Login'/>
+      </form>
+      </div>
+      <div class="column">
+      <h2>Logout from Session</h2>
+      <form action = "/logout" method = "POST">
+      <input type=submit value="Logout"/>
+      </form>
+      </div>
+      <div class="column">
+      <h2>Get Cookie</h2>
+      <form action = "/getcookie" method = "POST">
+      <input type=submit value="Get Cookie"/>
+      </form>
+      </div>
+      </div>
+      </div>
+      </div>
+    <div>
+      {% for member in group %}
+      <h1>hi</h1>
+      <p>By {{member.user}}</p>
+      {% endfor %}
     </div>
-  </div>
-  </div>
-</div>
-</div>
-
+    <p>{{usr}}</p>
     </body>
 </html>
