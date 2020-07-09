80e5468ac7bd1617123c733021aa6096c3d8f6ab
Anhtai <at@studioneun.de>
2020-07-09 11:57:59 +0200

    editing fronted

--- app/templates/index.php
+++ app/templates/index.php
@@ -8,12 +8,12 @@ $error='';
         <link rel="stylesheet" href="{{ url_for('static', filename='css/main.css') }}">
     </head>
     <body>
-      <div class="site-name">
+      <div class="header">
       <h1>Guess The Price</h1>
       </div>
-      <div class="section">
-      <div class="row, regbar">
-      <div class="column">
+      <div class="row">
+      <div class="left-section">
+      <div class="four-column">
       <h2>Session Creator</h2>
       <form action="#" method="POST" class=formholder"">
       <p>Name:</p>
@@ -21,7 +21,7 @@ $error='';
       <input type=submit value="Login"/>
       </form>
       </div>
-      <div class="column">
+      <div class="four-column">
       <h2>Cookie Creator</h2>
       <form action = "/setcookie" method = "POST">
       <p>Enter User-ID</p>
@@ -29,26 +29,33 @@ $error='';
       <input type = 'submit' value = 'Login'/>
       </form>
       </div>
-      <div class="column">
+      <div class="four-column">
       <h2>Logout from Session</h2>
       <form action = "/logout" method = "POST">
       <input type=submit value="Logout"/>
       </form>
       </div>
-      <div class="column">
+      <div class="four-column">
       <h2>Get Cookie</h2>
       <form action = "/getcookie" method = "POST">
       <input type=submit value="Get Cookie"/>
       </form>
       </div>
       </div>
-      </div>
-      </div>
+      <div class="right-section">
     <div class="group">
       {% for member in group %}
       <p>User {{member.user}}</p>
       {% endfor %}
     </div>
+    <div>
+      <form action = "/msg" method = "POST">
+        <input type=submit value="Send message"/>
+        <input type="text" name="msg"/>
+      </form>
+    </div>
+  </div>
     <p>{{usr}}</p>
+    </div>
     </body>
 </html>
