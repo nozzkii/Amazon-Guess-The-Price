41e39e069d61d97100a6855ac7f3728bb702a20d
Anhtai <at@studioneun.de>
2020-07-31 05:57:07 +0200

    created working nodejs container

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const element = <h1>Hello World!</h1>;
-console.log(element);
+const express = require("express");
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js app \n");
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
